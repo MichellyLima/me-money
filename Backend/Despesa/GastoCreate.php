8f35dfabef05ade442eb246796a76430dadfa6ef
rathalosmh123 <75038390+rathalosmh123@users.noreply.github.com>
2021-06-06 14:07:30 -0300

    Add files via upload

---
+++ Backend/Despesa/GastoCreate.php
@@ -0,0 +1,65 @@
+<?php
+    
+    
+
+        
+        //http://pgadmin.saude-go.net/browser/
+        $servername = "postgres-desenv.saude-go.net";
+        $database = "desenvolvimento";
+        $username = "gabrielmendonca";
+        $password = "rathalos2";
+        // Create connection
+        $db = pg_connect("host=$servername port=5432 dbname=$database user=$username password=$password" );
+        //$conn = mysqli_connect($servername, $username, $password, $database);
+        // Check connection
+        
+            
+           
+            
+            $id=($_REQUEST["id"]);
+            $categoria=($_REQUEST["categoria"]);
+            $valor=($_REQUEST["valor"]);
+            $descricao=($_REQUEST["descricao"]);
+     
+           
+            $idDespesa=($_REQUEST["idDespesa"]);
+            $mes=($_REQUEST["mes"]);
+
+
+             
+            
+                 
+
+
+
+            $sql = "INSERT INTO Gasto(id,categoria,valor,descricao) VALUES ('$id','$categoria','$valor','$descricao')";
+
+           
+            
+            pg_set_client_encoding($db, "utf8");
+
+            $execute=pg_query($db,$sql);
+        
+           
+
+            $sql = "INSERT INTO DespesaG(idDespesa,mes,idGasto) VALUES ('$idDespesa','$mes','$descricao','$id')";
+
+           
+            
+            pg_set_client_encoding($db, "utf8");
+
+            $execute=pg_query($db,$sql);
+
+
+          echo json_encode($vetor,JSON_UNESCAPED_UNICODE);
+            
+            
+            
+
+
+        
+
+    
+
+
+     
